0c7636676ef37a461b00449e433ac3371f8ec34d
robertmeggle <robertmeggle>
2010-09-09 14:19:34 +0000

    quckfix: Notice: Undefined index

--- include/care_api_classes/class_config.php
+++ include/care_api_classes/class_config.php
@@ -3,11 +3,11 @@ require_once($root_path.'include/care_api_classes/class_globalconfig.php');
 require_once($root_path.'include/care_api_classes/class_userconfig.php');
 
 
-if($_SESSION['sess_level2_logged'] && $_SESSION['sess_level2_user_id']) {
+if(isset($_SESSION['sess_level2_logged']) && isset($_SESSION['sess_level2_user_id'])) {
     $user_id = $_SESSION['sess_level2_user_id'];
-} elseif ($_SESSION['sess_level1_logged'] && $_SESSION['sess_level1_user_id']) {
+} elseif (isset($_SESSION['sess_level1_logged']) && isset($_SESSION['sess_level1_user_id'])) {
     $user_id = $_SESSION['sess_level1_user_id'];
-} elseif ($_SESSION['sess_user_id']) {
+} elseif (isset($_SESSION['sess_user_id'])) {
     $user_id = $_SESSION['sess_user_id'];
 } else {
     $user_id = $_COOKIE['ck_config'];
