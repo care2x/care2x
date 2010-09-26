0b1e20c71fb1b1abb1fe357ae026b32aaed2023c
mizuko <mizuko>
2010-09-26 15:39:38 +0000

    new installer template

--- installer/install.php
+++ installer/install.php
@@ -9,6 +9,8 @@ if (!isset($run_output)) {
 	$run_output = '';
 }
 $run_output .= $GLOBALS['INSTALLER']['ENGINE']->run();
+$smarty->assign('PHASE_LIST',$GLOBALS['INSTALLER']['ENGINE']->getPhaseList());
+$smarty->assign('PHASE_NUMBER',$GLOBALS['INSTALLER']['ENGINE']->getPhaseNumber());
 $smarty->assign('INSTALLER_PHASE', $GLOBALS['INSTALLER']['ENGINE']->getPhaseName());
 
 $output .= $smarty->fetch(Installer::getTemplatePath('header.tpl'));
