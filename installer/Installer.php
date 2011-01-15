eebd6c1be216de1d5afeccddca457663b4dea333
robertmeggle <robertmeggle>
2011-01-15 09:48:59 +0000

    assigning return values by reference is depriciated

--- installer/Installer.php
+++ installer/Installer.php
@@ -106,7 +106,7 @@ if(!file_exists($GLOBALS['INSTALLER']['CONFIG_FILE'])){
 	die();	
 }
 
-$GLOBALS['INSTALLER']['INSTALLER_CONFIG'] =& new InstallerConfig($GLOBALS['INSTALLER']['CONFIG_FILE']);
+$GLOBALS['INSTALLER']['INSTALLER_CONFIG'] = new InstallerConfig($GLOBALS['INSTALLER']['CONFIG_FILE']);
 if($GLOBALS['INSTALLER']['INSTALLER_CONFIG']->parse() === FALSE){
 	print("Installer Error: Error parsing config file {$GLOBALS['INSTALLER']['CONFIG_FILE']}<BR>\n");
 	print($GLOBALS['INSTALLER']['INSTALLER_CONFIG']->getErrorsHTML());
@@ -119,9 +119,9 @@ set_time_limit(0);
 //TODO Detect if session is already started, if so display error 
 session_start();
 if(isset($_REQUEST['restart_installer']) || !isset($_SESSION['INSTALLER']['ENGINE']) || !is_a($_SESSION['INSTALLER']['ENGINE'], 'InstallerEngine')){
-	$_SESSION['INSTALLER']['ENGINE'] =& new InstallerEngine($GLOBALS['INSTALLER']['INSTALLER_CONFIG']); 	
+	$_SESSION['INSTALLER']['ENGINE'] = new InstallerEngine($GLOBALS['INSTALLER']['INSTALLER_CONFIG']); 	
 }
-$GLOBALS['INSTALLER']['ENGINE'] =& $_SESSION['INSTALLER']['ENGINE'];
+$GLOBALS['INSTALLER']['ENGINE'] = $_SESSION['INSTALLER']['ENGINE'];
 
 if(isset($_REQUEST['previous_step'])){
 	$GLOBALS['INSTALLER']['ENGINE']->previousStep();
