078adf1c885c9e6bdfef4d7096d19330992844c2
sovello <sovellohpmgani@gmail.com>
2016-03-23 17:30:11 +0300

    defines the missing $target variable

--- modules/registration_admission/gui_bridge/default/gui_patient_reg_options.php
+++ modules/registration_admission/gui_bridge/default/gui_patient_reg_options.php
@@ -9,6 +9,8 @@ function Spacer()
                   width=5></TD></TR>
 <?php
 */}
+
+$target = $_REQUEST['target'];
 ?>
 <img <?php echo createComIcon($root_path,'angle_left_s.gif',0,'',TRUE); ?>>
 <br>
