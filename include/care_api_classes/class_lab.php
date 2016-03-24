461a3bb49f6e9250b4fb66744797025e751cac1c
sovello <sovellohpmgani@gmail.com>
2016-03-24 08:14:41 +0300

    change remove condition for group_id and order by sort_nr. group_id values are all strings and sort_nr column is currently not available. need to make sure sort_nr is created during installation and return this condition

--- include/care_api_classes/class_lab.php
+++ include/care_api_classes/class_lab.php
@@ -435,7 +435,8 @@ class Lab extends Encounter {
 		$cond = 'WHERE group_id = \'-1\' ';
 		if(empty($nr)) $cond .='';
 			else $cond .="AND nr='$nr'";
-		$this->sql="SELECT * FROM $this->tb_test_param $cond ORDER BY sort_nr";
+		//$this->sql="SELECT * FROM $this->tb_test_param $cond ORDER BY sort_nr";
+		$this->sql="SELECT * FROM $this->tb_test_param $cond";
 		if($this->tgroups=$db->Execute($this->sql)){
 		    if($this->rec_count=$this->tgroups->RecordCount()) {
 				return $this->tgroups;
@@ -451,7 +452,8 @@ class Lab extends Encounter {
 	*/
 	function TestActiveGroups(){
 		global $db;
-		$this->sql="SELECT * FROM $this->tb_test_param WHERE group_id = '-1' AND status NOT IN ($this->dead_stat) ORDER BY sort_nr";
+		//$this->sql="SELECT * FROM $this->tb_test_param WHERE group_id = '-1' AND status NOT IN ($this->dead_stat) ORDER BY sort_nr";
+		$this->sql="SELECT * FROM $this->tb_test_param WHERE status NOT IN ($this->dead_stat)";		
 		if($this->tgroups=$db->Execute($this->sql)){
 		    if($this->rec_count=$this->tgroups->RecordCount()) {
 				return $this->tgroups;
