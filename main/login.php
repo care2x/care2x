13f401ff1f90364a70c730d713e888835c4dc2d4
Tim Schofield <tim@weberpafrica.com>
2016-04-24 21:45:06 +0100

    Replace old error_reporting value

--- main/login.php
+++ main/login.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
+error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
 require('./roots.php');
 require($root_path.'include/core/inc_environment_global.php');
 require_once($root_path.'include/core/access_log.php');
