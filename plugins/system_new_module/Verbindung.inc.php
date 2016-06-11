67cd39fc3db0796e659e62aecf56dee52929af38
Tim Schofield <tim@weberpafrica.com>
2016-06-11 15:31:03 +0100

    Fix syntax error - 'break' not in the 'loop' or 'switch' context

--- plugins/system_new_module/Verbindung.inc.php
+++ plugins/system_new_module/Verbindung.inc.php
@@ -14,10 +14,9 @@ require_once("Verbindungs_Vars.php");
      }
   else{
       echo "Es konnte keine Verbindung zu ADODB aus Verbindung.inc.php erstellt werden.<br/>";
-			break;
 		  }
 //Herstellen einer Verbindung zu MySQL, und der Datenbank
-$conn = &ADONewConnection($db_art); 
+$conn = &ADONewConnection($db_art);
 //$conn->debug=true;
 $conn->PConnect($host,$user,$pwd,$dbname);
 
