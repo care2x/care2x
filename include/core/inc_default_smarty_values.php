6c0ad382e4f8b735a5657708f3d29fc3b797a08c
Tim Schofield <tim@weberpafrica.com>
2016-06-14 10:34:10 +0100

    Add an include file for default smarty values

---
+++ include/core/inc_default_smarty_values.php
@@ -0,0 +1,15 @@
+<?php
+
+$smarty->assign('bHideTitleBar',FALSE);
+$smarty->assign('Subtitle','');
+$smarty->assign('pbAux1','');
+$smarty->assign('pbAux2','');
+$smarty->assign('sCloseTarget','target="_parent"');
+$smarty->assign('sWarnText','');
+$smarty->assign('sMainFrameBlockData','');
+$smarty->assign('Name',"");
+$smarty->assign('sSubTitle','' );
+$smarty->assign('sMainDataBlock','');
+$smarty->assign('sOnLoadJs','');
+
+?>
