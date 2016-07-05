3ab7265fd3c1a58a571bcac10529f8bd5665e783
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-07-05 16:36:38 +0300

    fixes instantiation of an object. The old style allowed objects to be instantiated without the new Keyword which is required in php 7

--- modules/personell_admin/gui_bridge/default/gui_personell_register.php
+++ modules/personell_admin/gui_bridge/default/gui_personell_register.php
@@ -132,7 +132,7 @@ if(!$pid&&!$personell_nr){
 	$user_origin='admit';
 
 	require_once($root_path.'include/care_api_classes/class_gui_search_person.php');
-	$psearch =  GuiSearchPerson;
+	$psearch = new GuiSearchPerson();
 
 	$psearch->setSearchFile('personell_register_search.php');
 
